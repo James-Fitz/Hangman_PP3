db1ed69c1fd8b4a56387f17656dbde44edeb1962
James Fitzpatrick <james.fitz123@gmail.com>
2022-12-20 18:05:52 +0000

    Rearrange function to ensure main menu runs first

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import gspread, os
+import gspread, os, random
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -15,17 +15,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("hangman")
 
-easy_animal_words = SHEET.worksheet("easy_animals").get_all_values()
-intermediate_animal_words = SHEET.worksheet("intermediate_animals").get_all_values()
-hard_animal_words = SHEET.worksheet("hard_animals").get_all_values()
+easy_animals_words = SHEET.worksheet("easy_animals").get_all_values()
+intermediate_animasl_words = SHEET.worksheet("intermediate_animals").get_all_values()
+hard_animals_words = SHEET.worksheet("hard_animals").get_all_values()
 
-easy_brand_words = SHEET.worksheet("easy_brands").get_all_values()
-intermediate_brand_words = SHEET.worksheet("intermediate_brands").get_all_values()
-hard_brand_words = SHEET.worksheet("hard_brands").get_all_values()
+easy_brands_words = SHEET.worksheet("easy_brands").get_all_values()
+intermediate_brands_words = SHEET.worksheet("intermediate_brands").get_all_values()
+hard_brands_words = SHEET.worksheet("hard_brands").get_all_values()
 
-easy_country_words = SHEET.worksheet("easy_countries").get_all_values()
-intermediate_country_words = SHEET.worksheet("intermediate_countries").get_all_values()
-hard_country_words = SHEET.worksheet("hard_countries").get_all_values()
+easy_countries_words = SHEET.worksheet("easy_countries").get_all_values()
+intermediate_countries_words = SHEET.worksheet("intermediate_countries").get_all_values()
+hard_countries_words = SHEET.worksheet("hard_countries").get_all_values()
 
 
 def clear():
@@ -33,80 +33,95 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'echo -e \\\\033c')
 
 
+def run_game():
+    category = category_choice()
+    difficulty = difficulty_choice()
+    
+    
 def main_menu():
-    print(r"""
-  _   _                                                         
- | | | |   __ _   _ __     __ _   _ __ ___     __ _   _ __  
- | |_| |  / _` | | '_ \   / _` | | '_ ` _ \   / _` | | '_ \ 
- |  _  | | (_| | | | | | | (_| | | | | | | | | (_| | | | | |
- |_| |_|  \__,_| |_| |_|  \__, | |_| |_| |_|  \__,_| |_| |_|
-                          |___/                            
-    """)
+#    print(r"""
+#   _   _                                                         
+#  | | | |   __ _   _ __     __ _   _ __ ___     __ _   _ __  
+#  | |_| |  / _` | | '_ \   / _` | | '_ ` _ \   / _` | | '_ \ 
+#  |  _  | | (_| | | | | | | (_| | | | | | | | | (_| | | | | |
+#  |_| |_|  \__,_| |_| |_|  \__, | |_| |_| |_|  \__,_| |_| |_|
+#                           |___/                            
+#     """)
 
     
-    print(r"""
-             |\|
-             |\|
-             |\|
-             |\|
-             |\|
-             |\|
-             |_| 
-            (___)
-            (___)
-            (___)
-            (___)
-            (___)
-            (___)
-          ,(/   \),
-         ('/     \')
-        ('/       \')
-        |/|       |/|
-        |/|       |/|
-        |/|       |/|
-        (-\       /-)
-         \-\     /-/
-          \-\___/-/ 
-           '-----'
-    """)
+#     print(r"""
+#              |\|
+#              |\|
+#              |\|
+#              |\|
+#              |\|
+#              |\|
+#              |_| 
+#             (___)
+#             (___)
+#             (___)
+#             (___)
+#             (___)
+#             (___)
+#           ,(/   \),
+#          ('/     \')
+#         ('/       \')
+#         |/|       |/|
+#         |/|       |/|
+#         |/|       |/|
+#         (-\       /-)
+#          \-\     /-/
+#           \-\___/-/ 
+#            '-----'
+#     """)
     print("[1] Play Game")
     print("[2] Rules")
     print("[3] Credits")
     print("[0] Quit")
     
-main_menu()
+    print("Welcome to Hangman!")
+    selection = int(input("Please select a number from the main menu to continue...: \n"))
 
-print("Welcome to Hangman!")
-selection = int(input("Please select a number from the main menu to continue...: \n"))
+    while selection != 0:
+        if selection == 1:
+            clear()
+            print("Starting game...")
+            run_game()
+            break
+            # clears the console and runs the main game
+        elif selection == 2:
+            print("Rules for hangman are as follows: (input rules here)")
+            break
+            # print rules for hangman to console
+        elif selection == 3:
+            print("Credits : thank you to ....")
+            break
+            # print thank yous and credits to console
+        else:
+            print("Invalid choice, try again")
+            break
+            # will print when an invalid selection is chosen
+    
 
-
-while selection != 0:
-    if selection == 1:
-        clear()
-        print("Starting game...")
-        run_game()
-        break
-        # clears the console and runs the main game
-    elif selection == 2:
-        print("Rules for hangman are as follows: (input rules here)")
-        break
-        # print rules for hangman to console
-    elif selection == 3:
-        print("Credits : thank you to ....")
-        break
-        # print thank yous and credits to console
-    else:
-        print("Invalid choice, try again")
-        break
-        # will print when an invalid selection is chosen
-        
-        
 def category_choice():
     print("Please select a category...")
     print("[1] Animals")
     print("[2] Brands")
     print("[3] Countries")
     
+    selection = int(input("Please select a number from the menu to continue...: \n"))
+    
+    if selection == 1:
+        category = "animals"
+    elif selection == 2:
+        category = "brands"
+    elif selection == 3:
+        category = "countries"
+    else:
+        print("Invalid choice, try again")
+        category_choice()
+    
+    print(f"You selected { category.capitalize() }\n")
     return category
 
 
@@ -116,9 +131,20 @@ def difficulty_choice():
     print("[2] Intermediate - 6 letter word")
     print("[3] Hard - 7 letter word")
     
+    selection = int(input("Please select a number from the menu to continue...: \n"))
+    
+    if selection == 1:
+        difficulty = "easy"
+    elif selection == 2:
+        difficulty = "intermediate"
+    elif selection == 3:
+        difficulty = "hard"
+    else:
+        print("Invalid choice, try again")
+        difficulty_choice()
+    
+    print(f"You selected { difficulty.capitalize() }\n")
     return difficulty
 
-
-def run_game(category, difficulty):
-    category_choice()
-    difficulty_choice()
+  
+main_menu()
